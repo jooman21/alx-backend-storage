8753a9f0639bc9c32f785578f170beb83c343d76
jooman21 <jooman2121@gmail.com>
2023-01-18 03:57:54 +0300

    joo21

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""
+Module 12-log_stats.py
+"""
 from pymongo import MongoClient
 
-
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    nginx_collection = client.logs.nginx
 
+    total_logs = nginx_collection.count_documents({})
+    total_gets = nginx_collection.count_documents({"method": "GET"})
+    total_posts = nginx_collection.count_documents({"method": "POST"})
+    total_puts = nginx_collection.count_documents({"method": "PUT"})
+    total_patchs = nginx_collection.count_documents({"method": "PATCH"})
+    total_deletes = nginx_collection.count_documents({"method": "DELETE"})
+    total_status = nginx_collection.count_documents(
+                    {"method": "GET", "path": "/status"})
 
-if __name__ == '__main__':
-    run()
+    print("{} logs".format(total_logs))
+    print("Methods:")
+    print("\tmethod GET: {}".format(total_gets))
+    print("\tmethod POST: {}".format(total_posts))
+    print("\tmethod PUT: {}".format(total_puts))
+    print("\tmethod PATCH: {}".format(total_patchs))
+    print("\tmethod DELETE: {}".format(total_deletes))
+    print("{} status check".format(total_status))
